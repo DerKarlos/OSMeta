339ce131d0b2fb8fa01600ab40805a62fe7d7694
-karlos- <karlos@ac1000.de>
2022-04-18 18:09:42 +0200

    Rotating cube
    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # OSMeta
-OSM "Metaverse"
+ OSM Metaverse
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(movement)
+        .run();
+}
+
+#[derive(Component)]
+struct Movable;
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    })
+  //.insert(bevy::pbr::NotShadowCaster)
+    ;
+
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    })
+    .insert(Movable)
+  //.insert(bevy::pbr::NotShadowReceiver)
+    ;
+
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    // camera
+    commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+
+fn movement(
+    _input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Movable>>,
+) {
+    for mut transform in query.iter_mut() {
+        //println!("xx {:?}", transform);
+        let delta_y = 1.00*time.delta_seconds();
+        let delta_rotation = Quat::from_euler(EulerRot::ZYX, 0.0, delta_y, 0.0);
+        transform.rotation *= delta_rotation; // multiply! means addition
+        let scale = transform.scale.x * (1.+0.02*time.delta_seconds());  // just for fun
+        transform.scale = Vec3::new(scale,scale,scale);
+    }
+}
