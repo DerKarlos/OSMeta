06cc8834746b83664d8a679c5997fca70d45f62f
Oliver Scherer <github@oli-obk.de>
2023-11-27 00:16:07 +0100

    Look directly at the specified gps coordinate instead of just a corner of the tile that was selected

--- src/geopos.rs
+++ src/geopos.rs
@@ -1,4 +1,4 @@
-use super::utils::TileName;
+use bevy::prelude::*;
 use std::f32::consts::PI;
 
 /**
@@ -16,23 +16,28 @@ pub struct GeoPos {
 
 impl GeoPos {
     /**
-     * Calculate the tile-name(x/y) of a tile at this position
+     * Convert GPS coordinates to tile coordinates.
+     * We use the OSM naming for tiles:
+     * https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames.
+     * x and y relate to a lonitude and latitude position on Earth.
+     * In OSM he two values are only used as part of the filename of a tile.
+     * even if it is an x/y coordinate in numbers.
      * @param zoom  Zoom level of the OSM tile-name(x/y) system
-     * @return tile-name(x/y)
+     * @return coordinate in tile coordinates
      */
-    pub fn calc_tile_name(&self, zoom: u8) -> TileName {
+    pub fn to_tile_coordinates(&self, zoom: u8) -> Vec2 {
         let pow_zoom = 2_u32.pow(zoom.into()) as f32;
 
         // return
-        TileName {
+        Vec2 {
             // Longitude, (Längengrad) West/East "index"
-            x: ((self.lon + 180.) / 360. * pow_zoom).floor() as i32,
+            x: ((self.lon + 180.) / 360. * pow_zoom),
 
             // y: Latitude, (Breitengrad) Nort/South "index"
-            y: ((1. - ((self.lat * PI / 180.).tan() + 1. / (self.lat * PI / 180.).cos()).ln() / PI)
+            y: ((1.
+                - ((self.lat * PI / 180.).tan() + 1. / (self.lat * PI / 180.).cos()).ln() / PI)
                 / 2.
-                * pow_zoom)
-                .floor() as i32,
+                * pow_zoom),
             // The Nort/South y tile name part is not linear, the tiles gets stretched to the poles
             // to compensate the stretching if the stretching of the West/East projection
         }
