28e3230380b0248ddd83f67269c46119c264c114
Oliver Scherer <github@oli-obk.de>
2023-12-05 22:59:36 +0100

    Load a cloud and an earth texture from the internet

--- src/sun.rs
+++ src/sun.rs
@@ -3,7 +3,10 @@
 use bevy::{
     pbr::{CascadeShadowConfigBuilder, DirectionalLightShadowMap, NotShadowCaster},
     prelude::*,
-    render::texture::{ImageAddressMode, ImageSampler, ImageSamplerDescriptor},
+    render::{
+        render_resource::TextureFormat,
+        texture::{ImageAddressMode, ImageFilterMode, ImageSampler, ImageSamplerDescriptor},
+    },
 };
 use std::f32::consts::*;
 
@@ -14,9 +17,28 @@ pub struct Plugin;
 impl bevy::prelude::Plugin for Plugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(DirectionalLightShadowMap { size: 4096 })
+            .insert_resource(Clouds::default())
+            .insert_resource(Earth::default())
             .add_systems(Startup, setup)
-            .add_systems(Update, (animate_light_direction,));
+            .add_systems(
+                Update,
+                (
+                    animate_light_direction,
+                    set_clouds_transparent,
+                    set_earth_repeat,
+                ),
+            );
     }
+}
+
+#[derive(Resource, Default)]
+struct Clouds {
+    image: Option<Handle<Image>>,
+}
+
+#[derive(Resource, Default)]
+struct Earth {
+    image: Option<Handle<Image>>,
 }
 
 fn animate_light_direction(
@@ -38,7 +60,8 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     server: Res<AssetServer>,
-    mut images: ResMut<Assets<Image>>,
+    mut clouds: ResMut<Clouds>,
+    mut earth: ResMut<Earth>,
 ) {
     // Sun
     commands.spawn(DirectionalLightBundle {
@@ -70,6 +93,28 @@ fn setup(
         .unwrap(),
     );
 
+    // Clouds visible from earth and space
+    let image =
+        server.load("https://www.solarsystemscope.com/textures/download/2k_earth_clouds.jpg");
+    clouds.image = Some(image.clone());
+    commands.spawn((
+        PbrBundle {
+            mesh: mesh.clone(),
+            material: materials.add(StandardMaterial {
+                base_color: Color::WHITE,
+                unlit: true,
+                alpha_mode: AlphaMode::Blend,
+                cull_mode: None,
+                base_color_texture: Some(image),
+                ..default()
+            }),
+            transform: Transform::from_scale(Vec3::splat(EARTH_RADIUS + 100000.0)),
+            ..default()
+        },
+        NotShadowCaster,
+        GalacticGrid::ZERO,
+    ));
+
     // Sky
     commands.spawn((
         PbrBundle {
@@ -89,21 +134,14 @@ fn setup(
 
     let image =
         server.load("https://www.solarsystemscope.com/textures/download/2k_earth_daymap.jpg");
-    if let Some(image) = images.get_mut(image.clone()) {
-        image.sampler = ImageSampler::Descriptor(ImageSamplerDescriptor {
-            address_mode_u: ImageAddressMode::Repeat,
-            address_mode_v: ImageAddressMode::Repeat,
-            address_mode_w: ImageAddressMode::Repeat,
-            ..default()
-        });
-    }
+    earth.image = Some(image.clone());
 
     // ground
     commands.spawn((
         PbrBundle {
             mesh,
             material: materials.add(StandardMaterial {
-                base_color: Color::hex("533621").unwrap(),
+                base_color: Color::WHITE,
                 unlit: true,
                 cull_mode: None,
                 base_color_texture: Some(image),
@@ -117,4 +155,61 @@ fn setup(
         NotShadowCaster,
         GalacticGrid::ZERO,
     ));
+}
+
+fn set_clouds_transparent(
+    mut images: ResMut<Assets<Image>>,
+    mut clouds: ResMut<Clouds>,
+    server: Res<AssetServer>,
+) {
+    if let Some(handle) = clouds.image.take() {
+        use bevy::asset::LoadState::*;
+        if let Loaded | Failed = server.get_load_state(&handle).unwrap() {
+            let Some(image) = images.get_mut(&handle) else {
+                unreachable!()
+            };
+            info!("clouds made transparent and linear filtered");
+            *image = image.convert(TextureFormat::Rgba8UnormSrgb).unwrap();
+            for chunk in image.data.chunks_exact_mut(4) {
+                let [r, _g, _b, a] = chunk else {
+                    unreachable!()
+                };
+                *a = *r;
+            }
+            image.sampler = repeat_sampler();
+        } else {
+            clouds.image = Some(handle);
+        }
+    }
+}
+
+fn set_earth_repeat(
+    mut images: ResMut<Assets<Image>>,
+    mut earth: ResMut<Earth>,
+    server: Res<AssetServer>,
+) {
+    if let Some(handle) = earth.image.take() {
+        use bevy::asset::LoadState::*;
+        if let Loaded | Failed = server.get_load_state(&handle).unwrap() {
+            let Some(image) = images.get_mut(&handle) else {
+                unreachable!()
+            };
+            image.sampler = repeat_sampler();
+        } else {
+            earth.image = Some(handle);
+        }
+    }
+}
+
+fn repeat_sampler() -> ImageSampler {
+    ImageSampler::Descriptor(ImageSamplerDescriptor {
+        address_mode_u: ImageAddressMode::Repeat,
+        address_mode_v: ImageAddressMode::Repeat,
+        address_mode_w: ImageAddressMode::Repeat,
+        mag_filter: ImageFilterMode::Linear,
+        min_filter: ImageFilterMode::Linear,
+        mipmap_filter: ImageFilterMode::Linear,
+        anisotropy_clamp: 4,
+        ..default()
+    })
 }
