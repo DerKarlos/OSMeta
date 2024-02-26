0074bf72cf3a2c197979f16bff1c65ef1033cae9
Karl-Josef Adler <karl-josef.adler@web.de>
2024-02-26 16:38:05 +0100

    Update grid_cell.rs
    use super::big_space::GridCell;

--- src/big_space/grid_cell.rs
+++ src/big_space/grid_cell.rs
@@ -27,7 +27,7 @@ use super::precision::GridPrecision;
 /// define a type alias!
 ///
 /// ```
-/// # use crate::big_space::GridCell;
+/// # use super::big_space::GridCell;
 /// type GalacticGrid = GridCell<i64>;
 /// ```
 ///
